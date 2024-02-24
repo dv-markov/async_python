6ccf4bc2289072e54a593076c89d3edf49d03b3b
dv-markov <dmitry.v.markov@gmail.com>
2024-02-24 23:03:26 +0300

    more awaitable

---
+++ Lesson 3 - awaitable objects/Ex3.2 - event loop.py
@@ -0,0 +1,71 @@
+# Базовый цикл событий мог бы выглядеть следующим образом:
+# Данный код, всего лишь условность, реальный цикл событий выглядеть куда длинее и запутанее
+
+class SimpleEventLoop:
+    def __init__(self):
+        self.tasks = []  # Очередь задач
+
+    def add_task(self, task):
+        self.tasks.append(task)  # Добавление задачи в очередь
+
+    def run_forever(self):
+        while self.tasks:  # Выполнять, пока есть задачи
+            task = self.tasks.pop(0)  # Получить первую задачу
+            task()  # Выполнить задачу
+
+
+# Пример функций-задач
+def task1():
+    print("Выполняется задача 1")
+
+
+def task2():
+    print("Выполняется задача 2")
+
+
+# Создание и использование цикла событий
+loop = SimpleEventLoop()
+loop.add_task(task1)  # Добавить задачу 1 в цикл событий
+loop.add_task(task2)  # Добавить задачу 2 в цикл событий
+loop.run_forever()  # Запустить цикл событий
+
+
+import asyncio
+import random
+
+
+class Pizzeria:
+    def __init__(self, name):
+        self.name = name
+
+    async def make_pizza(self, order_id):
+        cook_time = random.randint(2, 5)      # случайное время готовки пиццы от 2 до 5 секунд
+        print(f'Пиццерия {self.name} начала готовить пиццу для заказа {order_id}.')
+        await asyncio.sleep(cook_time)        # ожидание пока пицца готовится
+        print(f'Пиццерия {self.name} закончила готовить пиццу для заказа {order_id}.')
+
+
+async def main():
+    pizzeria = Pizzeria("Тесто & Сыр")
+
+    # создание 5 заказов
+    tasks = [pizzeria.make_pizza(i) for i in range(1, 6)]
+
+    # запуск всех задач (заказов) в Event Loop
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
+
+
+# Выполнение синхронной функции внутри асинхронной
+async def async_func():
+    def sync_func():
+
+        # тут может быть любой синхронный код
+
+        return 42
+
+    result = sync_func()
+    print(result)
+
+asyncio.run(async_func())
---
+++ Lesson 3 - awaitable objects/Ex3.3 - await keyword.py
@@ -0,0 +1,82 @@
+import asyncio
+
+
+async def main():
+    print('Начинаем')
+    await asyncio.sleep(1)  # приостановка выполнения на 1 секунду
+    print('Закончили')
+
+# Запускаем асинхронную функцию
+asyncio.run(main())
+
+
+# Несколько асинхронных функций
+# Асинхронная функция, имитирующая чтение данных из файла
+async def read_data_from_file(filename):
+    print(f"Начинаем чтение из файла {filename}")
+
+    # Имитация задержки для чтения файла
+    await asyncio.sleep(2)  # Предположим, чтение занимает 2 секунды
+    print(f"Чтение из файла {filename} завершено")
+    return f"данные из {filename}"
+
+
+# Асинхронная функция, имитирующая отправку данных в интернет
+async def send_data_to_internet(data):
+    print("Начинаем отправку данных в интернет")
+
+    # Имитация задержки для отправки данных
+    await asyncio.sleep(3)  # Предположим, отправка занимает 3 секунды
+    print("Отправка данных в интернет завершена")
+
+
+# Главная асинхронная функция, которая управляет выполнением программы
+async def main():
+    filename = "example.txt"
+
+    # Чтение данных из файла
+    file_data = await read_data_from_file(filename)
+
+    # Отправка прочитанных данных в интернет
+    await send_data_to_internet(file_data)
+
+
+# Запуск асинхронной программы
+asyncio.run(main())
+
+
+import random
+
+
+async def boil_water(time: int):
+    print('Ставим чайник с водой на плиту...')
+    await asyncio.sleep(time)                               # Передаем управление плите
+    print('Чайник закипел!')
+
+
+async def chop_vegetables():
+    print('Начинаем нарезать овощи...')
+    await asyncio.sleep(random.randint(2, 4))               # Передаем управление нарезке овощей
+    print('Овощи нарезаны!')
+
+
+async def prepare_dinner():
+    await asyncio.gather(boil_water(5), chop_vegetables())   # Запускаем задачи параллельно
+
+
+asyncio.run(prepare_dinner())
+
+
+import aiohttp
+import asyncio
+
+async def fetch_data(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+async def main():
+    data = await fetch_data('http://python.org')
+    print(data)
+
+asyncio.run(main())
